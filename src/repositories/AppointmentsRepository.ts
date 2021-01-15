de45ef4bf4b67494e3d1bd2fb3340e1670545416
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2021-01-14 21:01:41 -0300

    :construction:

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,18 @@
+// Responsável por trabalhar com os dados, listar, criar, deletar e editar.
+
+import { EntityRepository, Repository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
+
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,40 @@
+// A rota deve se preocupar apenas em receber a requisição, chamar outro arquivo e devolver uma resposta
+
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
